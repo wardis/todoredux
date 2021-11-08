88c61da4ef19306e15acd1a8b3533e6ecdf90076
Wardi Soulaimana <wardis@candctc.com>
2021-11-08 13:40:23 +0000

    create app reducer

--- src/reducer.js
+++ src/reducer.js
@@ -10,8 +10,48 @@ const initialState = {
   },
 }
 
+function nextTodoId(todos) {
+  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
+  return maxId + 1
+}
+
 export default function appReducer(state = initialState, action) {
   switch (action.type) {
+    case 'todos/totoAdded':
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            id: nextTodoId(state.todos),
+            text: action.payload,
+            completed: false,
+          },
+        ],
+      }
+    case 'todos/todoToggled':
+      return {
+        ...state,
+        todos: state.todos.map((todo) => {
+          if (todo.id != action.payload) {
+            return todo
+          }
+
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        }),
+      }
+    case 'filters/statusFilterChanged': {
+      return {
+        ...state,
+        filters: {
+          ...state.filters,
+          status: action.payload,
+        },
+      }
+    }
     default:
       return state
   }
