7d6db1ec9939dbf238e8769b6b9ec3db9867e11d
Wardi Soulaimana <wardis@candctc.com>
2021-11-09 00:41:18 +0000

    remaining todos in footer

--- src/features/footer/Footer.js
+++ src/features/footer/Footer.js
@@ -1,6 +1,23 @@
 import React from 'react'
 
+import { StatusFilters } from '../filters/filtersSlice'
+
+const RemainingTodos = ({ count }) => {
+  const suffix = count === 1 ? '' : 's'
+
+  return (
+    <div className="todo-count">
+      <h5>Remaining Todos</h5>
+      <strong>{count}</strong> items{suffix} left
+    </div>
+  )
+}
+
 const Footer = () => {
+  const colors = []
+  const status = StatusFilters.All
+  const todosRemaining = 1
+
   return (
     <footer className="footer">
       <div className="actions">
@@ -8,6 +25,8 @@ const Footer = () => {
         <button className="button">Mark All Completed</button>
         <button className="button">Clear Completed</button>
       </div>
+
+      <RemainingTodos count={todosRemaining} />
     </footer>
   )
 }
