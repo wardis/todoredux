fce9dded10301c1fc3e26259ddd0ecd6503fd6a3
Wardi Soulaimana <wardis@candctc.com>
2021-11-09 02:04:13 +0000

    useDispatch to add new todo item

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -1,9 +1,20 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
 
 const Header = () => {
   const [text, setText] = useState('')
+  const dispatch = useDispatch()
 
   const handleChange = (e) => setText(e.target.value)
+
+  const handleKeyDown = (e) => {
+    const trimmedText = e.target.value.trim()
+
+    if (e.key === 'Enter' && trimmedText) {
+      dispatch({ type: 'todos/todoAdded', payload: trimmedText })
+      setText('')
+    }
+  }
 
   return (
     <header className="header">
@@ -12,6 +23,7 @@ const Header = () => {
         placeholder="What needs to be done?"
         value={text}
         onChange={handleChange}
+        onKeyDown={handleKeyDown}
       />
     </header>
   )
