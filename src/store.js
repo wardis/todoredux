7fc744714d63f449e67a7c7bb3ae861847bbccdd
Wardi Soulaimana <wardis@candctc.com>
2021-11-09 00:00:06 +0000

    redux-devtools-extension

--- src/store.js
+++ src/store.js
@@ -1,11 +1,11 @@
-import { createStore, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducer'
-import {
-  sayHiOnDispatch,
-  includeMeaningOfLife,
-} from './exampleAddons/enhancers'
+import { print1, print2, print3 } from './exampleAddons/middleware'
 
-const composedEnhancer = compose(sayHiOnDispatch, includeMeaningOfLife)
+const composedEnhancer = composeWithDevTools(
+  applyMiddleware(print1, print2, print3)
+)
 
 const store = createStore(rootReducer, composedEnhancer)
 
