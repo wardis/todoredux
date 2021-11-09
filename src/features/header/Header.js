9cb223192e025f70e763afc3224274f28c9ab4a3
Wardi Soulaimana <wardis@candctc.com>
2021-11-09 00:25:12 +0000

    header value setText

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Header = () => {
+  const [text, setText] = useState('')
+
+  const handleChange = (e) => setText(e.target.value)
+
   return (
     <header className="header">
-      <input className="new-todo" placeholder="What needs to be done?" />
+      <input
+        className="new-todo"
+        placeholder="What needs to be done?"
+        value={text}
+        onChange={handleChange}
+      />
     </header>
   )
 }
