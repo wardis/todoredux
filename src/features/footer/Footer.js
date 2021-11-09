44cc44fcb3bd75009bfb972826f32b4b63c3e2ad
Wardi Soulaimana <wardis@candctc.com>
2021-11-09 00:58:15 +0000

    footer status filters

--- src/features/footer/Footer.js
+++ src/features/footer/Footer.js
@@ -13,10 +13,35 @@ const RemainingTodos = ({ count }) => {
   )
 }
 
+const StatusFilter = ({ value: status, onChange }) => {
+  const renderedFilters = Object.keys(StatusFilters).map((key) => {
+    const value = StatusFilter[key]
+    const handleClick = () => onChange(value)
+    const className = value === status ? 'selected' : ''
+
+    return (
+      <li key={value}>
+        <button className={className} onClick={handleClick}>
+          {key}
+        </button>
+      </li>
+    )
+  })
+
+  return (
+    <div className="filters statusFilters">
+      <h5>Filter by Status</h5>
+      <ul>{renderedFilters}</ul>
+    </div>
+  )
+}
+
 const Footer = () => {
   const colors = []
   const status = StatusFilters.All
   const todosRemaining = 1
+
+  const onStatusChange = (status) => console.log('Status change: ', status)
 
   return (
     <footer className="footer">
@@ -27,6 +52,7 @@ const Footer = () => {
       </div>
 
       <RemainingTodos count={todosRemaining} />
+      <StatusFilter value={status} onChange={onStatusChange} />
     </footer>
   )
 }
